3a2f3c9d00ddfb96c10c5e531c56d8fe1408419e
Nicole Fronda <nfronda@gmail.com>
2022-12-12 12:47:47 -0800

    minor code cleanup and adding plots for convergence

--- quadratic_distributed.py
+++ quadratic_distributed.py
@@ -6,61 +6,66 @@ import matplotlib.pyplot as plt
 np.random.seed(42)
 m, n = 6, 6
 
-Q = np.random.randn(n, n)
+Q = np.random.randn(n, n)   # variable for quadratic objective
 Q = Q.T @ Q
 
-A = np.random.rand(m, n)
-A = A.T @ A
+a = np.random.rand(n)
 
-B = np.random.rand(m, n)
+alpha = 1   # parameter for fairness constraint
+beta = 1    # parameter for weighting of obstacle avoidance constraint
+gamma = 1   # parameter for smoothmin in calculating obstacle avoidance constraint
+kappa = 1   # parameter for weighting change in epsilon for local problem
+eps_bounds = 1  # bounds for eps in an iteration
 
-b = np.random.randn(m)
-c = np.random.randn(1)
+r = 5  # radius of circle
+c = np.array([1, 1])  # center of circle
+Ubox = 10  # box constraint
 
-print('Central problem with linear constraint')
-x = cp.Variable(n)
-objective = cp.Minimize(cp.quad_form(x, Q) + cp.sum_squares(x - cp.sum(x)/n))
-constraints = [B @ x <= b]
-prob = cp.Problem(objective, constraints)
-prob.solve()
-print("\nThe optimal value is", prob.value)
-print("A solution x is")
-print(x.value)
-print(np.dot(x.value, np.dot(Q, x.value)))
 
-# print('Central problem with quadratic constraint')
-# x = cp.Variable(n)
-# objective = cp.Minimize(cp.quad_form(x, Q))
-# constraints = [cp.quad_form(x, A) <= c, B @ x <= b]
+# print('Central problem')
+# c_stack = np.array([1, 1, 1, 1, 1, 1])  # center of circle replicated 3 times
+# u = cp.Variable(n)
+# u_min = cp.Variable(2)
+# objective = cp.Minimize(cp.quad_form(u, Q) + \
+# (alpha/n) * cp.sum_squares(u - cp.sum(u)/n) + \
+#  a.T @ u )#- \
+ #-(cp.square(cp.norm(2*u[0:2] - c)) - r*r))
+#  -1/gamma * cp.log(cp.exp(-gamma*(cp.square(cp.norm(2*u[0:2] - c)) - r*r)) +
+# cp.exp(-gamma*(cp.square(cp.norm(2*u[2:4] - c)) - r*r)) +
+# cp.exp(-gamma*(cp.square(cp.norm(2*u[4:] - c)) - r*r))) )
+
+# constraints = [B @ x <= b]
+# constraints = [u <= Ubox, -Ubox <= u]
 # prob = cp.Problem(objective, constraints)
-# prob.solve()
+# prob.solve(solver='CVXOPT', verbose=True)
 # print("\nThe optimal value is", prob.value)
-# print("A solution x is")
-# print(x.value)
-# print(np.dot(x.value, np.dot(Q, x.value)))
+# print("A solution u is")
+# print(u.value)
+# print(np.dot(u.value, np.dot(Q, u.value)))
 
 
-print('Distributed')
-alpha = 10
-eps_bounds = 1
-# init_x = np.random.randn(n)
-init_x = x.value
-agents_prev_sol = {0: cp.Parameter(2, value=np.zeros(2)),
+
+print('Distributed Problem, 3 Agents')
+agents_prev_eps = {0: cp.Parameter(2, value=np.zeros(2)),
 1: cp.Parameter(2, value=np.zeros(2)),
 2: cp.Parameter(2, value=np.zeros(2))
-# 3: cp.Parameter(2, value=np.zeros(2)),
-# 4: cp.Parameter(2, value=np.zeros(2))
 }
 
-agents_prev_x = {0: cp.Parameter(6, value=np.copy(init_x)),
-1: cp.Parameter(6, value=np.copy(init_x)),
-2: cp.Parameter(6, value=np.copy(init_x)),
-# 3: cp.Parameter(10, value=np.copy(init_x)),
-# 4: cp.Parameter(10, value=np.copy(init_x))
+init_u = np.random.randn(n)
+agents_prev_u = {0: cp.Parameter(6, value=np.copy(init_u)),
+1: cp.Parameter(6, value=np.copy(init_u)),
+2: cp.Parameter(6, value=np.copy(init_u)),
 }
 
 
 def generate_agent_variable_stacks():
+    """
+
+    :return:
+        agent_stacks, dictionary mapping agents to their local u parameter
+        agent_eps, dictionary mapping agents to their local eps variable
+
+    """
     agent_stacks = {}
     agent_eps = {}
 
@@ -80,28 +85,15 @@ def generate_agent_variable_stacks():
     # agent 3 variable
     eps3 = cp.Variable(2)
     eps3_zeros1 = cp.Parameter(4, value=np.zeros(4))
-    # eps3_zeros2 = cp.Parameter(4, value=np.zeros(4))
-    # agent_stacks[2] =  cp.hstack([eps3_zeros1, eps3, eps3_zeros2])
     agent_stacks[2] =  cp.hstack([eps3_zeros1, eps3])
     agent_eps[2] = eps3
 
-    # # agent 4 variable
-    # eps4 = cp.Variable(2)
-    # eps4_zeros1 = cp.Parameter(6, value=np.zeros(6))
-    # eps4_zeros2 = cp.Parameter(2, value=np.zeros(2))
-    # agent_stacks[3] = cp.hstack([eps4_zeros1, eps4, eps4_zeros2])
-    # agent_eps[3] = eps4
-    #
-    # # agent 4 variable
-    # eps5 = cp.Variable(2)
-    # eps5_zeros = cp.Parameter(8, value=np.zeros(8))
-    # agent_stacks[4] = cp.hstack([eps5_zeros, eps5])
-    # agent_eps[4] = eps5
-
     return agent_stacks, agent_eps
+
 
 # Distributed solve
 global_solution = []
+agent_solutions = {0: [], 1: [], 2: []}
 for iter in range(10):
     print('Iter {}'.format(iter))
     agent_stacks, agent_eps = generate_agent_variable_stacks()
@@ -109,45 +101,90 @@ for iter in range(10):
     for i in range(3):
         stack = agent_stacks[i]
 
-        other_agent_vals = agents_prev_x[i].value
+        other_agent_vals = agents_prev_u[i].value
         other_agent_vals = cp.Parameter(6, value=other_agent_vals)
         agent_vals_my_change = other_agent_vals + stack
 
-        J = cp.Parameter(n, value = 2*np.dot(Q, agents_prev_x[i].value))
+        # Gradient of Quadratic Objective
+        Jobj = cp.Parameter(n, value = 2*np.dot(Q, agents_prev_u[i].value))
 
-        objective = cp.Minimize(-1 * (stack.T @ (J +  \
-        # new term for variance of energy
-        2/(n-1) * cp.sum(agents_prev_x[i] - cp.sum(agents_prev_x[i])/n))) + \
-        alpha * cp.norm(agent_eps[i] - agents_prev_sol[i]) ** 2)
-        #constraints = [cp.quad_form(agent_vals_my_change, A) <= c, agent_eps[i] <= eps_bounds, (-1 * eps_bounds) <= agent_eps[i]]
-        constraints = [B @ agent_vals_my_change <= b, agent_eps[i] <= eps_bounds, (-1 * eps_bounds) <= agent_eps[i]]
+        # Gradient of Fairness Constraint (derivative of variance of energy)
+        if i == 0:
+            anrg = agents_prev_u[i].value[0:2]
+        elif i == 1:
+            anrg = agents_prev_u[i].value[2:4]
+        else:
+            anrg = agents_prev_u[i].value[4:]
+        fairness_partial = np.linalg.norm(anrg) - np.linalg.norm(np.mean(agents_prev_u[i].value.reshape(3, 2), axis=0))
+        Jfair = cp.Parameter(value=((2/(n-1)) * fairness_partial))
+
+
+        # Gradient of Obstacle Constraint (derivative of smooth min)
+        denom = np.exp(-gamma * np.linalg.norm(agents_prev_u[i].value[0:2] - c) ** 2) + \
+                np.exp(-gamma * np.linalg.norm(agents_prev_u[i].value[2:4] - c) ** 2) +\
+                np.exp(-gamma * np.linalg.norm(agents_prev_u[i].value[4:] - c) ** 2)
+        if i == 0:
+            # aobs = 2* -gamma**2 * agents_prev_u[i].value[0:2] * \
+            #        np.exp(-gamma * np.linalg.norm(agents_prev_u[i].value[0:2] - c))
+            aobs = 2* -gamma**2  * \
+                   np.exp(-gamma * np.linalg.norm(agents_prev_u[i].value[0:2] - c))
+        elif i == 1:
+            aobs = 2 * -gamma ** 2 * np.exp(
+                -gamma * np.linalg.norm(agents_prev_u[i].value[2:4] - c))
+        else:
+            aobs = 2 * -gamma ** 2 * np.exp(
+                -gamma * np.linalg.norm(agents_prev_u[i].value[4:] - c))
+        Jobs = cp.Parameter(value=aobs/denom)
+
+        objective = cp.Minimize(-1 * (stack.T @ (Jobj + alpha*Jfair + a + beta*Jobs)) +
+                                kappa * cp.norm(agent_eps[i] - agents_prev_eps[i]) ** 2
+        )
+
+        constraints = [agent_vals_my_change <= Ubox,
+                       -Ubox <= agent_vals_my_change,
+                       agent_eps[i] <= eps_bounds,
+                       (-1 * eps_bounds) <= agent_eps[i]]
+
         prob = cp.Problem(objective, constraints)
         prob.solve(verbose=False)
         if np.abs(prob.value) == np.inf:
                 print('Agent {} Local Problem is {}'.format(i, prob.status))
                 continue
-        # print('Agent {} Local Solution'.format(i))
-        # print(agent_eps[i].value)
-        # print(agent_stacks[i].value)
         solved_values = solved_values + agent_stacks[i].value
-        # print(solved_values)
+        agent_solutions[i].append(prob.value)
 
     for i in range(3):
-        agents_prev_x[i] = cp.Parameter(6, value=np.copy(solved_values))
-        agents_prev_sol[i] = agent_eps[i]
+        agents_prev_u[i] = cp.Parameter(6, value=init_u + np.copy(solved_values))
+        agents_prev_eps[i] = agent_eps[i]
 
     # print('Iter {} Global Solution'.format(iter))
     # print(solved_values)
     # print(np.dot(solved_values, np.dot(Q, solved_values)))
-    global_solution.append(np.dot(solved_values, np.dot(Q, solved_values)))
+    global_u = init_u + solved_values
+    global_solution.append(np.dot(global_u, np.dot(Q, global_u))
+                           + alpha * (1/(n-1)) * np.sum(np.linalg.norm(global_u) - np.linalg.norm(np.mean(global_u)))
+                           - beta * -gamma * np.log(np.exp(-gamma * np.linalg.norm(global_u[0:2] - c) ** 2) +
+                                                   np.exp(-gamma * np.linalg.norm(global_u[2:4] - c) ** 2) +
+                                                   np.exp(-gamma * np.linalg.norm(global_u[4:] - c) ** 2)
+                                                   )
+                           )
+    # global_solution.append(prob.value)
     # print(np.dot(A, solved_values) <= b)
 
-print('Final')
-print(solved_values)
-print(prob.value)
-print(np.dot(solved_values, np.dot(Q, solved_values)))
-print(np.dot(B, solved_values) <= b)
+# print('Final')
+# print(solved_values)
+# print(prob.value)
+# print(np.dot(init_u + solved_values, np.dot(Q, init_u + solved_values)))
 
 # Plot to see convergence
 plt.plot(global_solution)
+plt.title('Centralized Solution')
+plt.show()
+
+plt.clf()
+
+plt.plot(agent_solutions[0])
+plt.plot(agent_solutions[1])
+plt.plot(agent_solutions[2])
+plt.title('Agent Solutions')
 plt.show()
