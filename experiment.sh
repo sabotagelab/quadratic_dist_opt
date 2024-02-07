c8c2b97af645543c236a8e6b227530a7f1e374c5
Nicole Fronda <nfronda@gmail.com>
2024-02-06 19:36:45 -0800

    updating experiment script to work on hpc cluster

--- experiment.sh
+++ experiment.sh
@@ -8,6 +8,9 @@
 #SBATCH -c 28
 #SBATCH --mem=75G
 
+source /nfs/stak/users/frondan/.bashrc
+conda activate quad_dist_opt
+
 python mpc_test.py --exp_dir /3_central_fair_notion0 --trials 200 --notion 0 --N 3
 python mpc_test.py --exp_dir /3_central_fair_notion2 --trials 200 --notion 2 --N 3
 python mpc_test.py --exp_dir /3_central_fair_notion0_dist --trials 200 --notion 0 --dist --N 3
